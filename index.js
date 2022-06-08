ea26beba06e1a0039fe0e58f5859c608751e0c16
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-08 12:04:44 +0300

    commit tree 56ee78cad165929cb3912f8cfd980de6b84a5229

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 
 // Write your code here
-let num1=31
-let num2=2
-let num3=24
-let num4=5
-let multiply =num1*num2
-
+let num1=31, num2=2,num3=24, num4=5;
+let multiply=num1=num1*num2
 let random=Math.floor(Math.random()*10)+1;
 let mod=num3%num4;
+let max=Math.max(1,2,3,4,16,20,19);
 
-let max=Math.max(1,2,3,4,15,12,19,20)
